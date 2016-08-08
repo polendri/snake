278c9edb31ed6ed9d0688196cb0a6657bfb34e8a
Paul Hendry <paul@pshendry.com>
2016-08-08 13:17:27 -0700

    Use curses .box() for drawing arena border

--- src/display.py
+++ src/display.py
@@ -18,36 +18,13 @@ class CursesDisplay:
             x_offset)
 
         self.__draw_title()
-        self.__draw_arena_border((x_offset, y_offset))
+        self.stdscr.refresh()
 
     def __draw_title(self):
         title = 'SNAAAAKE'
         x_offset = (curses.COLS - len(title)) // 2
         y_offset = max(1, (curses.LINES - self.config.arena_size[1] - 2) // 4)
         self.stdscr.addstr(y_offset, x_offset, title)
-
-    def __draw_arena_border(self, offset):
-        x_min = offset[0] - 1
-        x_max = offset[0] + self.config.arena_size[0] + 1
-        y_min = offset[1] - 1
-        y_max = offset[1] + self.config.arena_size[1] + 1
-
-        # Draw corners
-        corner_char = '+'
-        self.stdscr.addch(y_min, x_min, corner_char)
-        self.stdscr.addch(y_min, x_max, corner_char)
-        self.stdscr.addch(y_max, x_min, corner_char)
-        self.stdscr.addch(y_max, x_max, corner_char)
-
-        # Draw edges
-        horizontal_edge_char = '-'
-        vertical_edge_char = '|'
-        horizontal_edge_str = horizontal_edge_char * (x_max - x_min - 1)
-        self.stdscr.addstr(y_min, x_min + 1, horizontal_edge_str)
-        self.stdscr.addstr(y_max, x_min + 1, horizontal_edge_str)
-        for i in range(y_min + 1, y_max):
-            self.stdscr.addch(i, x_min, vertical_edge_char)
-            self.stdscr.addch(i, x_max, vertical_edge_char)
 
     def draw(self, state):
         tile_to_display_char = {
@@ -69,6 +46,6 @@ class CursesDisplay:
                     # https://stackoverflow.com/questions/37648557/curses-error-add-wch-returned-an-error
                     pass
 
-        self.stdscr.refresh()
+        self.arena_win.box()
         self.arena_win.refresh()
-        self.arena_win.getch()
+        self.stdscr.getch()
